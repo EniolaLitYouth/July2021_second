31a48202e65151f924d12dac42d2688207256956
EniolaLitYouth <37642579+EniolaLitYouth@users.noreply.github.com>
2021-11-20 21:45:13 -0600

    Second commit

---
+++ src/main/java/veriousconcept_TestNG/TestNg.java
@@ -0,0 +1,22 @@
+package veriousconcept_TestNG;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+import java.util.Random;
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.Select;
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class TestNg { }
